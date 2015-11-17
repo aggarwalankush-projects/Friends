ce9eeb732b5e1d981c1e47240770e99c88b3f5ba
Ankush Aggarwal <ankushagg93@gmail.com>
2015-11-17 13:46:50 -0500

    added FriendsList Loader and Fragment

---
+++ app/src/main/java/com/example/anku/friends/FriendsListFragment.java
@@ -0,0 +1,51 @@
+package com.example.anku.friends;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+
+import java.util.List;
+
+public class FriendsListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<List<Friend>> {
+
+    private FriendsCustomAdaptor adaptor;
+    private static final int LOADER_ID = 1;
+    private ContentResolver contentResolver;
+    private List<Friend> friends;
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+        contentResolver=getActivity().getContentResolver();
+        adaptor = new FriendsCustomAdaptor(getActivity(), getChildFragmentManager());
+        setEmptyText("No Friends");
+        setListAdapter(adaptor);
+        setListShown(false);
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<List<Friend>> onCreateLoader(int id, Bundle args) {
+        contentResolver=getActivity().getContentResolver();
+        return new FriendsListLoader(getActivity(), contentResolver);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Friend>> loader, List<Friend> friends) {
+        adaptor.setData(friends);
+        this.friends=friends;
+        if(isResumed())
+            setListShown(true);
+        else
+            setListShownNoAnimation(true);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Friend>> loader) {
+        adaptor.setData(null);
+    }
+}
